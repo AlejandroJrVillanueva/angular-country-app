9bdf8f04b9467973b55633e2f4f619aebe3f05bb
AlejandroJrVillanueva <alejandrovillanuevareina@gmail.com>
2023-07-14 11:04:00 -0300

    add new method searchCountryByAlphaCode in my service

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -12,6 +12,18 @@ export class CountriesService {
   constructor(private http: HttpClient) {
   }
 
+  searchCountryByAlphaCode(code: string) : Observable<Country[]>{
+    const url = `${this.apiUrl}/alpha/${code}`;
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => {
+          console.log(error)
+          return of([])//of : devuelve un Observable del tipo que se pone en los parentesis
+        })
+      );
+  }
+
   searchCapital(term: string) : Observable<Country[]>{
     const url = `${this.apiUrl}/capital/${term}`;
 
