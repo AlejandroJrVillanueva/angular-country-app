df2a4f5186afa80cbc22f072526b98aa9d45c628
AlejandroJrVillanueva <alejandrovillanuevareina@gmail.com>
2023-07-14 11:15:08 -0300

    add argument for url for searchCountryByAlphaCode

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { CountriesService } from './../../services/countries.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-country-page',
+  selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService
+    ) {}
+
+  ngOnInit(): void {
+    // SIN destructuracion
+    // this.activatedRoute.params
+      // .subscribe((params) => {
+      //   console.log({params: params['id ']})
+      // });
+
+    // CON destructuracion
+    // this.activatedRoute.params
+    // .subscribe( ({id}) => {
+    //   this.countriesService
+    //   console.log({params: id})
+
+    // });
+
+    this.activatedRoute.params
+      .subscribe( ({id}) => {
+
+        this.countriesService.searchCountryByAlphaCode(id)
+          .subscribe( country => {
+            console.log({country})
+          });
+
+      });
+  }
 }
