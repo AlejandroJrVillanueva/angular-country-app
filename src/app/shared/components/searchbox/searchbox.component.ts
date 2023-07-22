23cf4ba935da393664133c4f12b21796b320ad1a
AlejandroJrVillanueva <alejandrovillanuevareina@gmail.com>
2023-07-22 17:21:55 -0300

    clean code in searchbox emit

--- src/app/shared/components/searchbox/searchbox.component.ts
+++ src/app/shared/components/searchbox/searchbox.component.ts
@@ -8,23 +8,14 @@ import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@
 })
 export class SearchboxComponent {
 
-  @ViewChild('txtSearchInput')
-  tagInput!: ElementRef<HTMLInputElement>;
-
   @Output()
   public onValue = new EventEmitter<string>();
 
   @Input()
   public placeholder : string = '';
 
-  emitSearchbox():void{
-    const newTag = this.tagInput.nativeElement.value;
-    this.onValue.emit(newTag);
-    this.tagInput.nativeElement.value = '';
+  emitSearchbox(value : string):void{
+    this.onValue.emit(value);
   }
-
-  // emitSearchbox(value : string):void{
-  //   this.onValue.emit(value);
-  // }
 
 }
