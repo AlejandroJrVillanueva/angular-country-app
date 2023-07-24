79a68318ab9af6bdd8844495e51152e7bd326654
AlejandroJrVillanueva <alejandrovillanuevareina@gmail.com>
2023-07-24 20:34:42 -0300

    add new type Region for searchByRegion

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe' |'Oceania'
+
 @Component({
   selector: 'countries-by-region-page',
-  templateUrl: './by-region-page.component.html',
-  styles: [
-  ]
+  templateUrl: './by-region-page.component.html'
 })
 export class ByRegionPageComponent {
 
-  countriesByRegion : Country [] = [];
+  countriesByRegion : Country[] = [];
+  public regions : Region[] = ['Africa','Americas','Asia','Europe','Oceania']
 
-  constructor(private countriesService: CountriesService) {
-  }
+  constructor(private countriesService: CountriesService) {}
 
   searchByRegion(term: string):void{
     this.countriesService.searchRegion(term)
